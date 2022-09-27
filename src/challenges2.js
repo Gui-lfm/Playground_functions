9b925e921ede9fa442b3d7ef953339fca0dc4111
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-09-27 19:04:09 -0300

    desafio 12 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,21 +1,28 @@
 // Desafio 11
 function generatePhoneNumber(array) {
-  let arrayVerificado =  verificaNumero(array);
-  if(typeof arrayVerificado === 'string'){
-    return arrayVerificado
+  let arrayVerificado = verificaNumero(array);
+  if (typeof arrayVerificado === 'string') {
+    return arrayVerificado;
   }
   let numero = criaNumero(arrayVerificado);
   return numero;
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let verificaA = lineA < lineB + lineC && lineA > Math.abs(lineB - lineC);
+  let verificaB = lineB < lineA + lineC && lineB > Math.abs(lineA - lineC);
+  let verificaC = lineC < lineA + lineB && lineC > Math.abs(lineA - lineB);
+
+  if (verificaA && verificaB && verificaC) {
+    return true
+  }
+  return false
 }
 
 // Desafio 13
 function hydrate() {
-  // seu código aqui
+  
 }
 
 module.exports = {
@@ -49,7 +56,7 @@ function verificaNumero(array) {
   return array;
 }
 
-function criaNumero(array){
+function criaNumero(array) {
   let numeroGerado = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
 
   return numeroGerado;
