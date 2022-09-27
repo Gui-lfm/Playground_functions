ee5377d318958808a9796fff25936e4114c38f1b
Guilherme Lucena <guilherme.lucena17@gmail.com>
2022-09-27 18:55:06 -0300

    desafio 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,11 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(array) {
+  let arrayVerificado =  verificaNumero(array);
+  if(typeof arrayVerificado === 'string'){
+    return arrayVerificado
+  }
+  let numero = criaNumero(arrayVerificado);
+  return numero;
 }
 
 // Desafio 12
@@ -18,3 +23,34 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
+function verificaNumero(array) {
+  if (array.length != 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    let count = 0;
+    if (array[i] < 0 || array[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    let numRepetido = array[i]
+
+    for (let j = 0; j < array.length; j++) {
+      if (numRepetido === array[j]) {
+        count += 1
+      }
+      if (count >= 3) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      }
+    }
+  }
+
+  return array;
+}
+
+function criaNumero(array){
+  let numeroGerado = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`
+
+  return numeroGerado;
+}
